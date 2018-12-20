89725b70acfe4dbebe9f25d7060a51a60776d53b
Blaise Clarke <blaiserclarke@gmail.com>
2018-12-20 13:44:32 -0800

    Finished first iteration of arrayMaxResult core, working on wrapper to direct console input into the function

--- dotnet401Prework/dotnet401Prework/Program.cs
+++ dotnet401Prework/dotnet401Prework/Program.cs
@@ -7,12 +7,32 @@ namespace dotnet401Prework
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            arrayMaxResultWrapper();
             Console.ReadLine();
         }
 
         static int arrayMaxResult(int[] targetArray, int chosenNumber)
         {
-            
+            //Given an array of integers and a chosen number, return a score consisting of the summation of the occurence of the number in the array. If no occurences exist, return 0.
+            //Sample Input: [6, 4, 4, 1, 3], 4 -- Sample Output: 8
+            //Sample Input: [1, 1, 3, 3, 5], 6 -- Sample Output: 0
+
+            int value = 0;
+            for(int i = 0; i < targetArray.Length; i++)
+            {
+                if (targetArray[i] == chosenNumber)
+                {
+                    value += chosenNumber;
+                }
+            }
+            return value;
+        }
+
+        static void arrayMaxResultWrapper()
+        {
+            Console.WriteLine("Please type in an array of five integers, and choose one of those integers.");
+            var userInput = Console.ReadLine();
+            Console.WriteLine(userInput.GetType());
         }
     }
 }
