a1b6d081c1609b4afb932ee1b7a490a94c10aafd
Blaise Clarke <blaiserclarke@gmail.com>
2018-12-20 13:34:35 -0800

    Initial creation of signature of arrayMaxResult

---
+++ dotnet401Prework/dotnet401Prework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace dotnet401Prework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        static int arrayMaxResult(int[] targetArray, int chosenNumber)
+        {
+            
+        }
+    }
+}
