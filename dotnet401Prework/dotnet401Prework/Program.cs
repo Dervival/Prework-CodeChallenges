7dd7f6405803710bcf2350cffcb178afb52b0c36
Blaise Clarke <blaiserclarke@gmail.com>
2018-12-26 18:49:23 -0800

    Finished challenge 3

--- dotnet401Prework/dotnet401Prework/Program.cs
+++ dotnet401Prework/dotnet401Prework/Program.cs
@@ -32,6 +32,12 @@ namespace dotnet401Prework
                         Console.WriteLine("Accessing Leap Year Calculator.");
                         isLeapYearWrapper();
                         break;
+                    case 3:
+                        Console.WriteLine("Accessing Perfect Sequence");
+                        perfectSequenceWrapper();
+                        break;
+                    case 4:
+                        break;
                     default:
                         validSelect = false;
                         Console.WriteLine("I'm sorry, I don't understand that input. Can you please try again?");
@@ -99,6 +105,82 @@ namespace dotnet401Prework
             else
             {
                 return false;
+            }
+        }
+
+        static bool isPerfectSequence(int[] array)
+        {
+            if(arraySum(array) == arrayMult(array))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static int arraySum(int[] array)
+        {
+            int sum = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+
+        static int arrayMult(int[] array)
+        {
+            int mult = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                mult *= array[i];
+            }
+            return mult;
+        }
+
+        static void perfectSequenceWrapper()
+        {
+            Console.WriteLine("Please type in an array of integers to determine if it's a perfect sequence.");
+            Console.WriteLine("For instance, typing in [1, 2, 3]) will calculate the result " + isPerfectSequence(new int[] { 1, 2, 3 }) + ".");
+            var userInput = Console.ReadLine();
+            bool containsNeg = userInput.Contains("-");
+            if (containsNeg)
+            {
+                Console.WriteLine("The array " + userInput + " is not a perfect sequence.");
+                return;
+            }
+            //Strip off that first bracket
+            if(userInput.Substring(0,1) == "[")
+            {
+                userInput = userInput.Substring(1);
+            }
+            //and its matching bracket
+            if(userInput.Substring(userInput.Length-1) == "]")
+            {
+                userInput = userInput.Substring(0, userInput.Length - 1);
+            }
+            //userInput = userInput.Substring(1, userInput.Length - 2);
+            var arrayInput = userInput.Split(",");
+            int[] intArray = new int[arrayInput.Length];
+            //Possible garbage between integers in the array that would be prepended to the int - keep trimming characters off the string array until it can be parsed into an integer
+            for (int i = 0; i < arrayInput.Length; i++)
+            {
+                bool parsedIndex = Int32.TryParse(arrayInput[i], out int number);
+                while (!parsedIndex)
+                {
+                    arrayInput[i] = arrayInput[i].Substring(1);
+                    parsedIndex = Int32.TryParse(arrayInput[i], out number);
+                }
+                intArray[i] = number;
+            }
+            if (isPerfectSequence(intArray))
+            {
+                Console.WriteLine("The array [" + userInput + "] is a perfect sequence.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("The array [" + userInput + "] is not a perfect sequence.");
+                return;
             }
         }
 
